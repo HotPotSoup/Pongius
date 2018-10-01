a0c4d00210193dabd9dd2ca6a9d149551cd558f1
HotPotSoup <gaosteven@rocketmail.com>
2018-10-01 16:02:04 -0400

    initial

---
+++ script.js
@@ -0,0 +1,79 @@
+   var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    var ballRadius = 10;
+    var x = canvas.width/2;
+    var y = canvas.height-30;
+    var dx = 2;
+    var dy = -2;
+    var paddleHeight = 75;
+    var paddleWidth = 10;
+    var paddleY = canvas.height/2 - paddleHeight/2;
+    var downPressed = false;
+    var upPressed = false;
+    var score = 0;
+    document.addEventListener("keydown", keyDownHandler, false);
+    document.addEventListener("keyup", keyUpHandler, false);
+    function keyDownHandler(e) {
+        if(e.keyCode == 40) {
+            downPressed = true;
+        }
+        else if(e.keyCode == 38) {
+            upPressed = true;
+        }
+    }
+    function keyUpHandler(e) {
+        if(e.keyCode == 40) {
+            downPressed = false;
+        }
+        else if(e.keyCode == 38) {
+            upPressed = false;
+        }
+    }
+    function drawBall() {
+        ctx.beginPath();
+        ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+    }
+    function drawPaddle() {
+        ctx.beginPath();
+        ctx.rect(0, paddleY, paddleWidth, paddleHeight);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+    }
+    function draw() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawBall();
+        drawPaddle();
+        document.getElementById("score").innerHTML = score;
+        if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+            dy = -dy;
+        }
+        if(x + dx > canvas.width-ballRadius) {
+            dx = -dx;
+        }
+        else if(x + dx < ballRadius) {
+            if(y > paddleY && y < paddleY + paddleHeight) {
+                dx = -dx;
+                score++;
+                dx = dx + 0.25;
+                dy = dy + 0.25;
+            }
+            else {
+                clearInterval(game);
+                alert("GAME OVER. SCORE = " + score);
+                document.location.reload();
+            }
+        }
+        if(downPressed && paddleY < canvas.height-paddleHeight) {
+            paddleY += 7;
+        }
+        else if(upPressed && paddleY > 0) {
+            paddleY -= 7;
+        }
+        x += dx;
+        y += dy;
+    }
+    var game = setInterval(draw, 10);
